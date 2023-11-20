35351d49f0c589adce8753094fe340dbd0fc9880
master <sungwoony@kakao.com>
2023-11-21 00:13:48 +0900

    feat. block 생성

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
-import {init, exit} from "myPackage";
+import crypto from 'crypto';
 
-init({
-    url: "true"
-})
+interface BlockShape {
+    hash:string;
+    prevHash:string;
+    height:number;
+    data:string;
 
-exit(1)
+}
 
-localStorage
-
-class Block {
-    constructor(private data: string) {}
-    static hello() {
-        return "hello"
+class Block implements BlockShape {
+    public hash: string;
+    constructor(
+        public prevHash:string,
+        public height:number,
+        public data: string
+    ) {
+        this.hash = Block.calculateHash(prevHash, height, data);
     }
+    static calculateHash(prevHash:string, height:number, data:string) {
+        const toHash = `${prevHash}${height}${data}`;
+
+        return crypto.createHash("sha256").update(toHash).digest("hex");
+
+    }
+
 }
