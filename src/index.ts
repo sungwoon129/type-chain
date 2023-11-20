c98b140f302165e215fd59aadad4248e0139bfe4
master <sungwoony@kakao.com>
2023-11-20 23:18:30 +0900

    js와 ts를 함께 사용하는 방법

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
-import {init, exit} from "myPackage";
+import {init, exit} from "./myPackage";
 
-init({
-    url: "true"
-})
 
 exit(1)
-
-localStorage
 
 class Block {
     constructor(private data: string) {}
