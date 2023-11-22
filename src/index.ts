191e8b2b80179e01d26c923df07716fadb64b498
master <sungwoony@kakao.com>
2023-11-22 22:24:59 +0900

    simple 블록체인 기능

--- src/index.ts
+++ src/index.ts
@@ -23,5 +23,32 @@ class Block implements BlockShape {
         return crypto.createHash("sha256").update(toHash).digest("hex");
 
     }
-
 }
+
+class BlockChain {
+    private blocks: Block[]
+    constructor() {
+        this.blocks = [];
+    }
+    private getPrevHash() {
+        if(this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length -1].hash
+    }
+
+    public addBlock(data:string) {
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(newBlock);
+
+    }
+
+    public getBlocks() {
+        return this.blocks;
+    }
+}
+
+const blockchain = new BlockChain();
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+
+console.log(blockchain.getBlocks());
